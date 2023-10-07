11c7003e907f2f3d4d4fd96ed58bc5c3ddccb05f
白云 <baiyun@ahpthd.com>
2023-10-07 18:10:15 +0800

    修改组件

---
+++ src/components/Banner/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    我是banner
+  </div>
+</template>
+
+<script lang='ts' setup>
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    我是header
+  </div>
+</template>
+
+<script lang='ts' setup>
+</script>
+
+<style lang='scss' scoped>
+
+</style>

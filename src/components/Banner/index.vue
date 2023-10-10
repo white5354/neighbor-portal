9a3143d93d6852a6aa2b26ae91035105c810e7f7
白云 <baiyun@ahpthd.com>
2023-10-10 16:31:03 +0800

    增加图片

--- src/components/Banner/index.vue
+++ src/components/Banner/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    我是banner
+    <el-carousel height="150px">
+      <el-carousel-item v-for="item in banners" :key="item">
+        <!-- <img :src="item.url" alt="" srcset=""> -->
+        {{ item.url }}
+
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script setup>
+import {reactive} from 'vue'
+const banners =  reactive([{
+  url: '1'
+}])
 </script>
 
 <style  scoped>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="text-primary bg-primary">
-    我是首页
+  <div class="text-primary">
+    <div class="top"></div>
+    <div>
+      <div>社区邻里，一呼百应！送取、帮办、解忧，让生活更轻松</div>
+      <div>轻松成为跑腿，让你的力量帮助邻里的生活变得更好</div>
+    </div>
+    <Banner></Banner>
   </div>
 </template>
 
-<script  setup>
-import {ref } from 'vue'
-const count = ref(0)
-
+<script setup>
+import Banner from "../../components/Banner/index.vue";
+import { ref } from "vue";
+const count = ref(0);
 </script>
 
-<style  scoped>
-
+<style scoped>
+.top {
+  background: url("@/assets/images/home/header-bg.png") center;
+  min-width: 1220px;
+}
 </style>
